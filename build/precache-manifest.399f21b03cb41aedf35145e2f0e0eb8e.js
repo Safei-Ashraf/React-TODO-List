7ea8986eee59d5364cd902f247bdbd3d594dcdb3
Safei Ashraf <44810632+Safei-Ashraf@users.noreply.github.com>
2020-07-16 22:08:22 +0200

    build folder

---
+++ build/precache-manifest.399f21b03cb41aedf35145e2f0e0eb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eed6f56a8ebb3fd6cfbc812730ce665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536749f5a894e86ea3c6",
+    "url": "/static/css/main.b1e1acb8.chunk.css"
+  },
+  {
+    "revision": "c9048f5b825aa5749250",
+    "url": "/static/js/2.d8649294.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8649294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536749f5a894e86ea3c6",
+    "url": "/static/js/main.10360047.chunk.js"
+  },
+  {
+    "revision": "b278acee63071185f4c1",
+    "url": "/static/js/runtime-main.ba9668b7.js"
+  }
+]);
